1a53971f77b0eb293df07c0257531a785412bc4f
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2024-07-10 07:36:59 -0500

    Update SpecNec_executable_streamlit.py

--- fun_SPEC_NEC/SpecNec_executable_streamlit.py
+++ fun_SPEC_NEC/SpecNec_executable_streamlit.py
@@ -11,6 +11,7 @@ st.image(image_path, use_column_width=True)
 # Título de la aplicación
 st.markdown("<h4 style='text-align: center;'>Simple App: Seismic Response Spectrum [Normative Ecuadorian Spectrum]</h4>", unsafe_allow_html=True)
 
+st.markdown("**Hola Xavier**")
 
 st.markdown(
     """
