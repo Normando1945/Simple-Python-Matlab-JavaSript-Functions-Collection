6e69f65a81c21ff03a9e4858b5e9704e93935d28
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2024-07-13 17:47:21 -0500

    Update BNewmark_app.py

--- fun_BNewmark/Simple_App/BNewmark_app.py
+++ fun_BNewmark/Simple_App/BNewmark_app.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import butter, lfilter
-import glob
-import os
+
 
 ## # line of run c:\users\normando\appdata\local\packages\pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0\localcache\local-packages\python310\scripts\streamlit.exe run BNewmark_app.py
 
