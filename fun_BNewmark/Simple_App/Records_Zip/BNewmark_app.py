ec64ef55e2be457430d692ce0b871c592447c893
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2024-07-14 14:46:39 -0500

    Update BNewmark_app.py

--- fun_BNewmark/Simple_App/Records_Zip/BNewmark_app.py
+++ fun_BNewmark/Simple_App/Records_Zip/BNewmark_app.py
@@ -83,7 +83,7 @@ st.markdown(
     This simple application performs step-by-step calculations of the displacement, velocity, and acceleration response of a single-degree-of-freedom (SDOF) structure, whether conservative or non-conservative, subjected to ground motion using the Newmark method.
     
     ##### 📥 **Seismic Records**
-    You can download .AT2 files with examples of real earthquakes in the required format from the following [**repository**](https://github.com/Normando1945/Simple-Python-Matlab-JavaSript-Functions-Collection/tree/main/fun_BNewmark/Simple_App/Records_Zip)
+    This simple application utilizes the PEER format, enabling you to download seismic records directly from the [**PEER Ground Motion Database**](https://ngawest2.berkeley.edu/). Additionally, you can download .AT2 files with real earthquake examples in the required format from the following [**repository**](https://github.com/Normando1945/Simple-Python-Matlab-JavaSript-Functions-Collection/tree/main/fun_BNewmark/Simple_App/Records_Zip)
     
     '''
 )
