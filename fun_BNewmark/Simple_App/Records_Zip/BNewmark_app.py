96728ed29d91edf577dc42ddb70c9267a53bd7c0
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2024-08-22 09:18:29 -0500

    Update BNewmark_app.py

--- fun_BNewmark/Simple_App/Records_Zip/BNewmark_app.py
+++ fun_BNewmark/Simple_App/Records_Zip/BNewmark_app.py
@@ -157,7 +157,6 @@ with col1:
         [![ResearchGate](https://img.shields.io/badge/-ResearchGate-00CCBB?style=social&logo=researchgate)](https://www.researchgate.net/profile/Carlos-Celi)
         [![Google Scholar](https://img.shields.io/badge/-Google%20Scholar-4285F4?style=social&logo=google)](https://scholar.google.com.ec/citations?hl=es&user=yR4Gz7kAAAAJ)
         [![YouTube](https://img.shields.io/badge/-YouTube-FF0000?style=social&logo=youtube)](https://www.youtube.com/@CCeli1945)
-        <a href="Carlos Celi:normando1945@gmail.com"><img alt="Email" src="https://img.shields.io/badge/Email-normando1945@gmail.com-blue?style=flat&logo=gmail"></a>
         """
     )
 with col2:
