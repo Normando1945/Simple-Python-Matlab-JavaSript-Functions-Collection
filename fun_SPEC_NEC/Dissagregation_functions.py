7b0f71b12c690f8a8ce4ddb6093c1205a5721f1e
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2024-08-30 09:24:14 -0500

    Update Dissagregation_functions.py

--- fun_SPEC_NEC/Dissagregation_functions.py
+++ fun_SPEC_NEC/Dissagregation_functions.py
@@ -417,7 +417,7 @@ def Code_dissagregation(df, LAT, LON, file_csv_name, folder_path, project_name):
         st.image(images["image4"], caption='Sample Disaggregation Plot')
         st.image(images["image5"], caption='Sample Disaggregation Plot')
 
-    return TRT_Rmeans_Mmeans_IMT
+    return TRT_Rmeans_Mmeans_IMT, all_means
 
 
 
